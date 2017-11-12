2439c460cb0532aadc72bcfa59a126f47c83cb87
Zoltán Tóth <zshun86@gmail.com>
2017-11-12 15:01:34 +0100

    fix: express server should handle path location strategy

--- scripts/server.js
+++ scripts/server.js
@@ -4,6 +4,10 @@ const app = express();
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(path.join(__dirname, '..', 'demo/dist')));
+app.all('*', function (req, res) {
+    res.status(200).sendFile(path.join(__dirname, '..', 'demo/dist', 'index.html'));
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 4200);
